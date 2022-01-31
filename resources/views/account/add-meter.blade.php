f8c7555d3168aa0c1de30e7a4ef552b3e89323fc
Kings Israel <milimokings@gmail.com>
2022-01-31 15:26:10 +0300

    Updated add-meter view

--- resources/views/account/add-meter.blade.php
+++ resources/views/account/add-meter.blade.php
@@ -23,46 +23,18 @@
       </div>
       <div class="col-lg-6 col-md-6 col-sm-12">
          <label for="f_name">IMEI</label>
-         <input id="token-base-amount" class="input-bordered input-with-hint" name="imei" type="text" value="861245050814630">
+         <input id="token-base-amount" class="input-bordered input-with-hint" name="imei" type="text" >
       </div>
       <div class="col-lg-6 col-md-6 col-sm-12">
          <label for="f_name">ICCID</label>
-         <input id="token-base-amount" class="input-bordered input-with-hint" name="iccid" type="text" value="89254021264260877372">
+         <input id="token-base-amount" class="input-bordered input-with-hint" name="iccid" type="text">
       </div>
       <div class="col-lg-6 col-md-6 col-sm-12">
          <label for="f_name">Address</label>
-         <input id="token-base-amount" class="input-bordered input-with-hint meter-address" name="address" value="002021090001" type="text">
-         {{-- <input id="token-base-amount" class="input-bordered input-with-hint meter-address" name="address" type="text"> --}}
+         <input id="token-base-amount" class="input-bordered input-with-hint meter-address" name="address" type="text">
       </div>
    </div>
    <br>
    <button type="submit" class="btn btn-primary">Add</button>
 </form>
-@push('scripts')
-   <script>
-      // $('.meter-address').on('keyup', function() {
-      //    // console.log($(this).val());
-      //    let address = formatAddress($(this).val())
-
-      //    console.log(address.reverse().toString())
-      // })
-
-      // function formatAddress(address, n=2) {
-      //    // let newAddress = address.replace(/(\d{2})/g, '$1 ').replace(/(^\s+|\s+$)/,' ')
-      //    var ret = [];
-      //    var i;
-      //    var len;
-
-      //    for(i = 0, len = address.length; i < len; i += n) {
-      //       ret.push(address.substr(i, n))
-      //    }
-
-      //    return ret
-      //    // let newAddress = address.replace(/(.{2})/g, "$1 ")
-      //    // let completeAddress = newAddress.split(' ').reverse().toString().replace(',', ' ')
-      //    // console.log(completeAddress);
-      //    // return completeAddress
-      // }
-   </script>
-@endpush
 @endsection
