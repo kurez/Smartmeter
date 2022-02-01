80a5ef29f24cb23813255162a9a190e741336b31
Kings Israel <milimokings@gmail.com>
2022-02-01 08:21:00 +0300

    Update

--- app/Http/Controllers/MeterController.php
+++ app/Http/Controllers/MeterController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Meter;
+use App\Rules\PhoneNumber;
+use Illuminate\Support\Facades\Validator;
 
 class MeterController extends Controller
 {
